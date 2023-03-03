90c24c25e7e790bbd2adfa3c93c8b966d460084e
hadihaider055 <haiderhadi055@gmail.com>
2023-03-04 00:14:41 +0500

    session 8: Javascript and Basics of React

---
+++ session 8 (JS)/app.js
@@ -0,0 +1,78 @@
+// function calcSum(num1, num2) {
+//   return num1 - num2;
+// }
+
+// let getSum = calcSum(5, 3);
+
+// console.log(getSum);
+
+// const color = "purple";
+
+// switch (color) {
+//   case "red":
+//     console.log("Your fav color is red");
+//     break;
+
+//   case "yellow":
+//     console.log("Your fav color is yellow");
+//     break;
+
+//   default:
+//     console.log("Invalid color");
+// }
+
+// for(variable initialization; condition; increment/decrement)
+
+// var i = 1;
+// while (i < 1) {
+//   console.log("The value of i is ", i);
+//   i++;
+// }
+
+// do {
+//   console.log("The value of i is ", i);
+//   i++;
+// } while (i < 1);
+
+// function buttonHoverState() {
+//   console.log("Button Hovered!");
+// }
+
+// const buttonInactive = () => {
+//   console.log("Button Inactive!");
+// };
+
+// function showAlert() {
+//   alert("Hello world!");
+// }
+
+// function getFirstName() {
+//   document.getElementById("firstName");
+//   document.getElementsByClassName;
+//   console.log();
+// }
+
+// console.log(document.getElementById("firstName"));
+
+// const student1 = {
+//     name: "Hadi Haider",
+//     rNo: "381279",
+//     cgpa: 4
+// }
+// const student2 = {
+//     name: "Hadi",
+//     rNo: "381279",
+//     cgpa: 4
+// }
+// const students = [student1, student2]
+// const student1Entries = Object.entries(student1)
+// console.log(student1Entries)
+// console.log(Object.fromEntries(student1Entries))
+// console.log(Object.keys(student1))
+
+// const pathname = window.location.pathname;
+// console.log(pathname);
+
+// function changeUrl() {
+//   window.location.href = "https://gdscuit.web.app/";
+// }
